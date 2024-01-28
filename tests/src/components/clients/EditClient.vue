61060cedf5edcdb4b646cbecfab2d288ab92a983
Stephan Johnson <stephanjohnson@users.noreply.github.com>
2024-01-28 07:25:41 +0200

    updates

--- tests/src/components/clients/EditClient.vue
+++ tests/src/components/clients/EditClient.vue
@@ -1,36 +1,36 @@
 <i18n lang="yaml" src="./EditClient.i18n.yaml" />
 
 <script setup lang="ts">
-import type { ClientDto } from '~/structs/dto/clients/client.dto';
-import type { TitleDto } from '~/structs/dto/titles/title.dto';
+import type { ClientDto } from "~/structs/dto/clients/client.dto";
+import type { TitleDto } from "~/structs/dto/titles/title.dto";
 
-import type { ChangeNameClientCommand } from '~/structs/dto/clients/change-name-client-command.dto';
+import type { ChangeNameClientCommand } from "~/structs/dto/clients/change-name-client-command.dto";
 
 const { t } = useI18n();
 
 const props = defineProps<{
-  clientId: string,
+  clientId: string;
 }>();
-	
+
 const clientsServiceProxy = useClientsServiceProxy();
 const titlesServiceProxy = useTitlesServiceProxy();
 
 // Queries
-const { 
-  data: editClientGetClientByIdQueryData, 
-  pending: editClientGetClientByIdQueryPending, 
-  error: editClientGetClientByIdQueryError , 
-  execute: editClientGetClientByIdQueryExecute, 
-  refresh: editClientGetClientByIdQueryRefresh, 
-  status: editClientGetClientByIdQueryStatus 
+const {
+  data: editClientGetClientByIdQueryData,
+  pending: editClientGetClientByIdQueryPending,
+  error: editClientGetClientByIdQueryError,
+  execute: editClientGetClientByIdQueryExecute,
+  refresh: editClientGetClientByIdQueryRefresh,
+  status: editClientGetClientByIdQueryStatus,
 } = await clientsServiceProxy.getClientByIdQuery(props.clientId);
-const { 
-  data: titleSelectGetTitlesQueryData, 
-  pending: titleSelectGetTitlesQueryPending, 
-  error: titleSelectGetTitlesQueryError , 
-  execute: titleSelectGetTitlesQueryExecute, 
-  refresh: titleSelectGetTitlesQueryRefresh, 
-  status: titleSelectGetTitlesQueryStatus 
+const {
+  data: titleSelectGetTitlesQueryData,
+  pending: titleSelectGetTitlesQueryPending,
+  error: titleSelectGetTitlesQueryError,
+  execute: titleSelectGetTitlesQueryExecute,
+  refresh: titleSelectGetTitlesQueryRefresh,
+  status: titleSelectGetTitlesQueryStatus,
 } = await titlesServiceProxy.getTitlesQuery();
 
 // Model
@@ -62,7 +62,6 @@ watchEffect(async () => {
 
 // Commands
 const changeNameClientCommand = async () => {
-
   const command: ChangeNameClientCommand = {
     id: model.id,
     firstName: model.firstName,
@@ -71,18 +70,18 @@ const changeNameClientCommand = async () => {
   };
 
   const id = props.clientId;
-          
-	const changeNameClientCommand = await clientsServiceProxy.changeNameClientCommand(id, command);
-};
 
+  const changeNameClientCommand =
+    await clientsServiceProxy.changeNameClientCommand(id, command);
+};
 
 // TitleSelect Options
 const titleSelectOptions = computed(() => {
   const options: { value: string; label: string }[] = [];
   if (titleSelectGetTitlesQueryData.value) {
     titleSelectGetTitlesQueryData.value.forEach((item: TitleDto) => {
-      const name = item.name || '';
-      const code = item.code || '';
+      const name = item.name || "";
+      const code = item.code || "";
       const mappedExpression = `${name} (${code})`;
 
       options.push({
@@ -105,30 +104,32 @@ onMounted(() => {
     :title="t('personalInformation.title')"
     :description="t('personalInformation.description')"
   >
-
     <template #actions>
       <button
         type="button"
         @click="changeNameClientCommand()"
         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
-        {{ t('personalInformation.actions.save') }}
+        {{ t("personalInformation.actions.save") }}
       </button>
     </template>
-    
-     <ui-input-select 
-       v-model="model.titleId" 
-       :label="t('personalInformation.titleSelect.label')"
-       :options="titleSelectOptions"
-      />
-     <ui-input-textbox 
-       v-model="model.firstName" 
-       :label="t('personalInformation.firstNameTextBox.label')" />
-     <ui-input-textbox 
-       v-model="model.lastName" 
-       :label="t('personalInformation.lastNameTextBox.label')" />
-     <ui-input-textbox 
-       v-model="model.otherNames" 
-       :label="t('personalInformation.otherNamesTextBox.label')" />
+
+    <ui-input-select
+      v-model="model.titleId"
+      :label="t('personalInformation.titleSelect.label')"
+      :options="titleSelectOptions"
+    />
+    <ui-input-textbox
+      v-model="model.firstName"
+      :label="t('personalInformation.firstNameTextBox.label')"
+    />
+    <ui-input-textbox
+      v-model="model.lastName"
+      :label="t('personalInformation.lastNameTextBox.label')"
+    />
+    <ui-input-textbox
+      v-model="model.otherNames"
+      :label="t('personalInformation.otherNamesTextBox.label')"
+    />
   </ui-editor-section>
 </template>
